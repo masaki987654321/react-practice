e6052751c462c36b6bdaf8ae0354bf3fd20a0060
kaneko <bull.31415@gmail.com>
2020-06-03 16:19:47 +0900

    react練習その３
    firebaseを用いたチャットアプリの作成
    
    環境構築終了

---
+++ Chat/chat_app/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+import {connect} from 'react-redux';
+
+import AlignItemsList from './components/AlignItemsList';
+import ButtonAppBar from './components/ButtonAppBar.js';
+import TextInput from './components/TextInput.js';
+
+import {firebaseDb} from 'firebase';
+
+const messagesRef = firebaseDb.ref('messages');
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ButtonAppBar />
+        <div className='App'>
+          <AlignItemsList />
+          <TextInput value="メッセージを入力" />
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
